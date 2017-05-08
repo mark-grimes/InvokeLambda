1c11a17969c1c64376f0172b14dd536ebbdb8ae1
Mark Grimes <kknb1056@gmail.com>
2017-05-08 19:52:33 +0100

    Add the UI interface elements

--- InvokeLambda/InvokeLambda/MainPage.xaml.cs
+++ InvokeLambda/InvokeLambda/MainPage.xaml.cs
@@ -13,5 +13,15 @@ namespace InvokeLambda
 		{
 			InitializeComponent();
 		}
+
+		private void onButtonClicked(object sender, EventArgs e)
+		{
+			output_information.Text = "Lambda invocation requested";
+		}
+
+		private void onAcquireIdentity(object sender, EventArgs e)
+		{
+			output_information.Text = "Identity pool changed to " + input_identityPool.Text;
+		}
 	}
 }
