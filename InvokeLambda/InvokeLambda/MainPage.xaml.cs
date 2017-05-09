38f41cd9ef45b09e92cf9b12367a971860dd5050
Mark Grimes <kknb1056@gmail.com>
2017-05-09 16:53:13 +0100

    Add the code to send the Lambda invocation

--- InvokeLambda/InvokeLambda/MainPage.xaml.cs
+++ InvokeLambda/InvokeLambda/MainPage.xaml.cs
@@ -9,6 +9,25 @@ namespace InvokeLambda
 {
 	public partial class MainPage : ContentPage
 	{
+		private Amazon.CognitoIdentity.CognitoAWSCredentials credentials = null;
+
+		private void getCredentials()
+		{
+			Amazon.RegionEndpoint region = endpointFromName(input_IdentityPoolRegion.Items[input_IdentityPoolRegion.SelectedIndex]);
+			credentials = new Amazon.CognitoIdentity.CognitoAWSCredentials(region.SystemName + ":" + input_identityPool.Text, region);
+		}
+
+		private Amazon.RegionEndpoint endpointFromName(string regionName)
+		{
+			foreach (var endpoint in Amazon.RegionEndpoint.EnumerableAllRegions)
+			{
+				if (endpoint.SystemName == regionName) return endpoint;
+			}
+
+			// regionName not found
+			return null;
+		}
+
 		public MainPage()
 		{
 			InitializeComponent();
@@ -16,12 +35,54 @@ namespace InvokeLambda
 
 		private void onButtonClicked(object sender, EventArgs e)
 		{
-			output_information.Text = "Lambda invocation requested";
+			if (credentials == null) getCredentials();
+			Amazon.RegionEndpoint endpoint = endpointFromName(input_region.Items[input_region.SelectedIndex]);
+			Amazon.Lambda.AmazonLambdaClient client = new Amazon.Lambda.AmazonLambdaClient(credentials, endpoint);
+
+			var invokeRequest = new Amazon.Lambda.Model.InvokeRequest { FunctionName = input_functionName.Text, InvocationType = "RequestResponse", Payload = input_payload.Text };
+			System.Threading.Tasks.Task<Amazon.Lambda.Model.InvokeResponse> responseTask = client.InvokeAsync(invokeRequest);
+
+			responseTask.ContinueWith((response) =>
+			{
+				string statusText = "";
+				string informationalText = "Internal error: Unhandled branch point"; // Should always be set in the next "if"
+				Xamarin.Forms.Color resultColour = Xamarin.Forms.Color.Red;
+
+				if (response.IsCanceled) { statusText = "Cancelled"; informationalText = ""; resultColour = Xamarin.Forms.Color.Black; }
+				else if (response.IsFaulted) { statusText = "Faulted"; informationalText = response.Exception.Message; }
+				else if (response.IsCompleted)
+				{
+					statusText = "Finished";
+					var responseReader = new System.IO.StreamReader(response.Result.Payload);
+					informationalText = responseReader.ReadToEnd();
+					resultColour = Xamarin.Forms.Color.Black;
+				}
+
+				// This continuation is not run on the main UI thread, so need to set up
+				// another task perform the UI changes on the correct thread.
+				Xamarin.Forms.Device.BeginInvokeOnMainThread(() =>
+				{
+					output_status.Text = statusText;
+					output_information.Text = informationalText;
+					output_status.TextColor = resultColour;
+					output_information.TextColor = resultColour;
+				});
+			});
+
+			output_status.Text = "Running...";
+			output_status.TextColor = Xamarin.Forms.Color.Black;
+			output_information.Text = "";
 		}
 
-		private void onAcquireIdentity(object sender, EventArgs e)
+		private void onIdentityPoolChanged(object sender, TextChangedEventArgs e)
 		{
-			output_information.Text = "Identity pool changed to " + input_identityPool.Text;
+			credentials = null;
+			input_identityPool.TextColor = Xamarin.Forms.Color.Black; // In case some earlier try turned it red
+		}
+
+		private void onIdentityPoolRegionChanged(object sender, EventArgs e)
+		{
+			credentials = null;
 		}
 	}
 }
